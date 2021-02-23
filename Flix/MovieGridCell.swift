b4dcdaa22d6e72444f80089410b11c2bb602159c
Nour Elabbasy <nour102600@gmail.com>
2021-02-23 13:55:15 -0500

    Flix part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Nour Elabbasy  on 2/22/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet var posterView: UIImageView!
+    
+}
